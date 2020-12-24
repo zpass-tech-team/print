6034109f309b172a014e13054954e5accae2f9fb
Sowmya Ujjappa Banakar <43202851+sowmya695@users.noreply.github.com>
2020-12-24 12:52:21 +0530

    deployment issue fixed

--- src/main/java/io/mosip/print/PrintPDFApplication.java
+++ src/main/java/io/mosip/print/PrintPDFApplication.java
@@ -3,6 +3,8 @@ package io.mosip.print;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -10,7 +12,8 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import io.mosip.kernel.cbeffutil.impl.CbeffImpl;
 import io.mosip.kernel.core.cbeffutil.spi.CbeffUtil;
 
-@SpringBootApplication(exclude = { CacheAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, 
+        HibernateJpaAutoConfiguration.class, CacheAutoConfiguration.class})
 public class PrintPDFApplication {
 
 	@Bean
